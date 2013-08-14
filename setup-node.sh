d2d8ce273894db90d5d36e5c8c9c392eb278f5e0
kushalj <kushal@elysian.net>
2013-08-14 21:09:42 +0000

    setup-node fixed... something. can't remember what :(

--- setup-node.sh
+++ setup-node.sh
@@ -3,6 +3,8 @@ git clone https://github.com/kushalj/setup-node
 cd setup-node
 ./setup.sh
 cd $HOME/scripts
+echo ""
 echo 'You may want to run:'
 echo 'source git_setup.sh "Name Surname" "email@address.com"'
+echo ""
 
