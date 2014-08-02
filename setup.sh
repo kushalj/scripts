a288088e3d02c6311fc88d0da4de306a24b2ae81
Kushal <kushal@elysian.net>
2014-08-02 18:37:23 +0100

    setup amends

--- setup.sh
+++ setup.sh
@@ -1,15 +1,22 @@
 sudo apt-get install git
 sudo apt-get install mercurial 
 sudo apt-get install curl
-sudo apt-get install vim-gtk
 sudo apt-get install tmux
+# ruby setup
 sudo apt-get install ruby
 sudo gem install tmuxinator 
 
+# go setup
+sudo apt-get install golang
+mkdir -p ~/go
+
+# setup vim
+sudo apt-get install vim-gtk
 mkdir -p ~/.vim/autoload ~/.vim/bundle && \
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+sudo apt-get install build-essential cmake python-dev
 
-# let's get git and mercurial setup
+# git and mercurial setup
 source ./setup-git.sh
 source ./setup-mercurial.sh
 
